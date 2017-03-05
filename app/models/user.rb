ff906325624ae4e72c8fc8eed07b5b14cb7ace37
shoMMKS <sho15-kanan-town-1739@ezweb.ne.jp>
2017-03-05 21:51:37 +0900

    Create alert system and liblary db

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   validates :name, presence: true
-  
+
   def set_image(file)
       if !file.nil?
           file_name = file.original_filename
