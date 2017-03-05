6f95e3b2eab318a34e752c58068daab26f007b9f
shoMMKS <sho15-kanan-town-1739@ezweb.ne.jp>
2017-03-05 13:36:41 +0900

    Complete foundation of user system

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,30 @@
 class UserController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_user, only:[:show, :edit, :update]
   def show
+  end
+  
+  def edit
+  end
+  
+  def update
+      file = params[:user][:image]
+      @user.set_image(file)
+      
+      if @user.update(user_params)
+          redirect_to @user, notice: 'ユーザー情報が更新されました'
+          else
+          render :edit
+      end
+  end
+  
+  private
+  
+  def set_user
+     @user = User.find(params[:id])
+  end
+  
+  def user_params
+      params.require(:user).permit(:name, :email)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303013102) do
+ActiveRecord::Schema.define(version: 20170305021453) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170303013102) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
